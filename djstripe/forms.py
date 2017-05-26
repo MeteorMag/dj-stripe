ef6980f2f8c012fbf22ee9d5f518ec010a841eb6
Joe Weiss <joe.weiss@gmail.com>
2017-05-26 11:29:24 -0400

    CMS-1919 Make forms fully dynamic

--- djstripe/forms.py
+++ djstripe/forms.py
@@ -12,7 +12,14 @@ from .models import return_plan_choices
 
 
 class PlanForm(forms.Form):
+
     plan = forms.ChoiceField(choices=return_plan_choices())
+
+    def __init__(self, *args, **kwargs):
+        super(PlanForm, self).__init__(*args, **kwargs)
+        # Make form fully dynamic.
+        self.fields['plan'] = forms.ChoiceField(choices=return_plan_choices())
+        super(PlanForm, self).full_clean()
 
 
 class CancelSubscriptionForm(forms.Form):
